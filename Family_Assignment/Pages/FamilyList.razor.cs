8dcded758b03baff02892c71b2537b5eb83255c1
hanch <jurco10510@gmail.com>
2021-10-12 15:39:05 +0200

    FamilyList and FamilyView

---
+++ Family_Assignment/Pages/FamilyList.razor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Models;
+
+namespace Family_Assignment.Pages
+{
+    public partial class FamilyList:ComponentBase
+    {
+        private IList<Family> allFamilies;
+        private IList<Family> toShowFamilies;
+        
+        protected override async Task OnInitializedAsync()
+        {
+            allFamilies = fileReader.GetAllFamilies();
+            toShowFamilies = allFamilies;
+        }
+
+        public void NavigateToFamily()
+        {
+            NavMgr.NavigateTo("FamilyView");
+        }
+        
+    }
+}
---
+++ Family_Assignment/Pages/FamilyView.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Family_Assignment.Pages
+{
+    public partial class FamilyView:ComponentBase
+    {
+        
+    }
+}
