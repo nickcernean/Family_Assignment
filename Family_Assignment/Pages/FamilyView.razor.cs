95759881f5380082712d4b2d3553bfec8d7afa3b
hanch <jurco10510@gmail.com>
2021-10-12 16:31:28 +0200

    Family view part of it

--- Family_Assignment/Pages/FamilyView.razor.cs
+++ Family_Assignment/Pages/FamilyView.razor.cs
@@ -1,9 +1,25 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Models;
 
 namespace Family_Assignment.Pages
 {
     public partial class FamilyView:ComponentBase
     {
+        [Parameter]
+        public string StreetName { get; set; }
+        [Parameter]
+        public int HouseNumber { get; set; }
+
+        private IList<Adult> allAdults;
+        private IList<Adult> toShowAdults;
         
+        protected override async Task OnInitializedAsync()
+        {
+            allAdults = fileReader.GetFamily(StreetName,HouseNumber).Adults;
+            toShowAdults = allAdults;
+        }
     }
 }
