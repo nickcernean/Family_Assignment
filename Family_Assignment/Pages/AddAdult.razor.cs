ec1b26988b5e46f962ed96b6ab53922462478f6c
hanchry <jurco10510@gmail.com>
2021-10-30 18:43:48 +0200

    adding of person fix

--- Family_Assignment/Pages/AddAdult.razor.cs
+++ Family_Assignment/Pages/AddAdult.razor.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using Models;
 
 namespace Family_Assignment.Pages
