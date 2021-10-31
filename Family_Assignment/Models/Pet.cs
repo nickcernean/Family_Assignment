79dd4bdd72c00f1930c68f3bff6447f185b17003
hanchry <jurco10510@gmail.com>
2021-10-31 11:41:43 +0100

    add pet fix

--- Family_Assignment/Models/Pet.cs
+++ Family_Assignment/Models/Pet.cs
@@ -1,12 +1,17 @@
-﻿using System.Globalization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
 namespace Models
 {
     public class Pet
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Species { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public int Age { get; set; }
     }
 }
